64db2dc1559440e6dfb4de8502db73e5fa7608a0
Odragde <odragde@live.com>
2021-02-05 13:33:30 -0300

    optimized exercise 18

--- exercise18.py
+++ exercise18.py
@@ -2,32 +2,22 @@ import random
 
 
 def run(): 
-    a = str(random.randint(1000,9999))
-    r_num = []
+    g_number = str(random.randint(1000,9999))
+    cows_bulls = [0,0]
     c = 1
-    for i in a:
-        r_num.append(int(i))
-    user = input("Enter a 4 digit number: ")
-    while user != a:
+    while cows_bulls[0] != 4:
+        cows_bulls = [0,0]
+        u_number = input("Enter a 4 digit number: ")
+        for i in range(0,4):
+            if u_number[i] == g_number[i]:
+                cows_bulls[0] +=1
+            elif g_number[i] in u_number:
+                cows_bulls[1] +=1
+        print(f"{cows_bulls[0]} cows and {cows_bulls[1]} bulls")
         c +=1
-        user_num = []
-        bulls = 0
-        cows = 0
-        for i in user:
-            user_num.append(int(i))
-        for k in range(0,4):
-            if user_num[k] == r_num[k]:
-                cows +=1
-                continue
-            else:
-                for i in user_num:
-                    if i == r_num[k]:
-                        bulls += 1
-        if user_num == r_num: break
-        print(f"{cows} cows and {bulls} bulls")
-        user = input("Enter a 4 digit number: ")
-    print(f"You win!, and only took you {c} attempts.")
-
+    if cows_bulls[0] == 4:
+        print(f"You win! {g_number} was the number, and it only took you {c} attempts.")
+    print(u_number)
 
 if __name__=="__main__":
     run()
