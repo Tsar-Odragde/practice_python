0fecdafa42fb53ba80085b3ce616aba6db1ec52e
Odragde <odragde@live.com>
2021-02-05 21:33:49 -0300

    created a function for checking the user number

--- exercise18.py
+++ exercise18.py
@@ -1,6 +1,15 @@
 import random
 
 
+def cows_bulls_checker(cows_bulls, g_number, u_number):
+    for i in range(0,4):
+        if u_number[i] == g_number[i]:
+            cows_bulls[0] +=1
+        elif g_number[i] in u_number:
+            cows_bulls[1] +=1
+    return cows_bulls
+
+
 def run(): 
     g_number = str(random.randint(1000,9999))
     cows_bulls = [0,0]
@@ -8,16 +17,12 @@ def run():
     while cows_bulls[0] != 4:
         cows_bulls = [0,0]
         u_number = input("Enter a 4 digit number: ")
-        for i in range(0,4):
-            if u_number[i] == g_number[i]:
-                cows_bulls[0] +=1
-            elif g_number[i] in u_number:
-                cows_bulls[1] +=1
+        cows_bulls = cows_bulls_checker(cows_bulls, g_number, u_number)
         print(f"{cows_bulls[0]} cows and {cows_bulls[1]} bulls")
         c +=1
     if cows_bulls[0] == 4:
         print(f"You win! {g_number} was the number, and it only took you {c} attempts.")
-    print(u_number)
+
 
 if __name__=="__main__":
     run()
