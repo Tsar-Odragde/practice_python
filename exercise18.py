2e7f034ef4fcce7476bd726c5080e4e8a7d3f2c6
Odragde <odragde@live.com>
2021-02-06 01:27:41 -0300

    added some spacing when running the exercise 18

--- exercise18.py
+++ exercise18.py
@@ -5,17 +5,17 @@ def cows_bulls_checker(cows_bulls, g_number):
     c = 0
     while cows_bulls[0] != 4:
         cows_bulls = [0,0]
-        u_number = input("Enter a 4 digit number: ")
+        u_number = input("Enter a 4 digit number: \n")
         if u_number == "exit": return False, c
         elif len(u_number) != 4:
             print(f"ERROR: {u_number} not a valid input.\n")
-            u_number = input("Please enter a 4 digit number: ")
+            u_number = input("Please enter a 4 digit number: \n")
         for i in range(0,4):
             if u_number[i] == g_number[i]:
                 cows_bulls[0] +=1
             elif g_number[i] in u_number:
                 cows_bulls[1] +=1
-        print(f"{cows_bulls[0]} cows and {cows_bulls[1]} bulls")
+        print(f"{cows_bulls[0]} cows and {cows_bulls[1]} bulls.\n")
         c +=1
     return True, c
 
