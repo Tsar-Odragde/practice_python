ecb1b64403e6b50bf4ef86f29f77d224762ce6bd
Odragde <odragde@live.com>
2021-02-06 05:22:29 -0300

    exercise 19 final7

--- exercise19.py
+++ exercise19.py
@@ -3,11 +3,10 @@ import requests
 
 
 def run():
-    url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
-    r_url = requests.get(url).text
-    soup = BeautifulSoup(r_url, "html.parser")
-    for n in soup.find_all(name="p"):
-        print(n.get_text())
+    soup = BeautifulSoup(requests.get("https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture").text, "html.parser")
+    for n in soup.find_all(name="div", class_="body"):
+        for i in n.find_all(name="p"):
+            print(i.get_text())
 
 
 if __name__=="__main__":
