6a73ff267374ac7b1240881af6bdf0a7bc1464ab
Odragde <odragde@live.com>
2021-02-07 01:29:27 -0300

    .get_text() instead of .string in exercise 17

--- exercise17.py
+++ exercise17.py
@@ -5,11 +5,11 @@ import requests
 def run():
     soup = BeautifulSoup((requests.get("https://www.nytimes.com/")).text, "html.parser")
     for n in soup.find_all(class_="styln-hp-briefing-subhead css-pcs5ck"):
-        print(n.string)
+        print(n.get_text())
     for n in soup.find_all(class_="e1lsht870"):
-        print(n.string)
-    for n in soup.find_all(class_="e1voiwgp0")[2:22]:
-        print(n.string)
+        print(n.get_text())
+    for n in soup.find_all(class_="e1voiwgp0"):
+        print(n.get_text())
     
 
 if __name__=="__main__":
