d505e1930c21baaa8c4cddf0217a061f81c5ed0c
Odragde <odragde@live.com>
2021-02-07 00:44:36 -0300

    exercise 20 with binary search

--- exercise20.py
+++ exercise20.py
@@ -1,12 +1,25 @@
 import random
 
 
+def binary_search(a,b):
+    s_i = 0
+    e_i = len(a) - 1
+    c = 0
+    while True:
+        m_i = int((e_i + s_i)/2)
+        if m_i < s_i or m_i > e_i or m_i < 1 or c > 99: return False
+        m_e = a[m_i]
+        if m_e == b: return True
+        elif b > m_e: s_i = m_i
+        else: e_i = m_i
+        c +=1
+
+
 def run():
     a = sorted([random.randint(0,100) for _ in range(random.randint(0,100))])
     b = int(input("Please enter a number in the range 0-99: "))
-    bol = b in a
-    if bol: print(True)
-    else: print(False)
+    print(binary_search(a,b))
+    print(a)
 
 
 if __name__=="__main__":
