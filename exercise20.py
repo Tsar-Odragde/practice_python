d63116e05649651f0eb9abeef79a55ca3507aaa6
Odragde <odragde@live.com>
2021-02-24 17:38:37 -0300

    Ejercicio 20 final, removí el contador, optimicé el código y cambié el nombre de las variables para mejorar el entendimiento

--- exercise20.py
+++ exercise20.py
@@ -1,24 +1,26 @@
 import random
 
 
-def binary_search(a,b):
-    s_i = 0
-    e_i = len(a) - 1
-    c = 0
+def binary_search(random_list,number):
+    start = 0
+    end = len(random_list) - 1
     while True:
-        m_i = int((e_i + s_i)/2)
-        if m_i < 1 or c > 99: return False
-        if a[m_i] == b: return True
-        elif b > a[m_i]: s_i = m_i
-        else: e_i = m_i
-        c += 1
+        middle = int((end + start)/2)
+        if (end - start)/2 < 1: 
+            return False
+        if random_list[middle] == number: 
+            return True
+        elif random_list[middle] < number: 
+            start = middle
+        else: 
+            end = middle
 
 
 def run():
-    a = sorted([random.randint(0,100) for _ in range(random.randint(0,100))])
-    print(a)
-    b = int(input("Please enter a number in the range 0-99: "))
-    print(binary_search(a,b))
+    random_list = sorted([random.randint(0,100) for _ in range(random.randint(0,100))])
+    print(random_list)
+    number = int(input("Please enter a number in the range 0-99: "))
+    print(binary_search(random_list, number))
 
 
 if __name__=="__main__":
